ab78a97a1138467b96fcb91130a1efa47d378969
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-01-25 10:03:19 +0800

    支持调整输出格式的一些参数。

--- Converter/Controls/TranscodeConfigPicker.xaml.cs
+++ Converter/Controls/TranscodeConfigPicker.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Converter.Classes;
 using Converter.ViewModels;
+using Windows.UI;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -30,24 +31,108 @@ namespace Converter.Controls
                 new TranscodeConfigListItem(){DisplayName="ALAC",Configuration=TranscodeConfiguration.CreateAlac() },
                 new TranscodeConfigListItem(){DisplayName="FLAC",Configuration=TranscodeConfiguration.CreateFlac() }
             };
+            AudioPanel.Visibility = _selectedConfiguration?.Audio == null ?
+                Visibility.Collapsed : Visibility.Visible;
+            VideoPanel.Visibility = _selectedConfiguration?.Video == null ?
+                Visibility.Collapsed : Visibility.Visible;
         }
 
+        TranscodeConfiguration _selectedConfiguration;
         public TranscodeConfiguration SelectedConfiguration
         {
             get
             {
-                return (transcodeConfigComboBox.SelectedItem as TranscodeConfigListItem)?.
-                    Configuration;
+                if (_selectedConfiguration == null)
+                    return null;
+                if (_selectedConfiguration.Audio != null)
+                {
+                    _selectedConfiguration.Audio.BitsPerSample = audioBitsPerSample;
+                    _selectedConfiguration.Audio.SampleRate = audioSampleRate;
+                    _selectedConfiguration.Audio.Bitrate = audioBitrate;
+                }
+                if (_selectedConfiguration.Video != null)
+                {
+                    _selectedConfiguration.Video.Height = videoHeight;
+                    _selectedConfiguration.Video.Width = videoWidth;
+                    _selectedConfiguration.Video.Bitrate = videoBitrate;
+                }
+                return _selectedConfiguration;
             }
         }
 
         private void TranscodeConfigComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            SelectedConfigChanged?.Invoke(this, SelectedConfiguration);
+            _selectedConfiguration =
+                (transcodeConfigComboBox.SelectedItem as TranscodeConfigListItem)?.
+                    Configuration;
+            SelectedFormatChanged?.Invoke(this, SelectedConfiguration);
+            AudioPanel.Visibility = _selectedConfiguration?.Audio == null ?
+                Visibility.Collapsed : Visibility.Visible;
+            VideoPanel.Visibility = _selectedConfiguration?.Video == null ?
+                Visibility.Collapsed : Visibility.Visible;
         }
 
         public delegate void ConfigChangedEventHandler(TranscodeConfigPicker sender, TranscodeConfiguration newConfig);
 
-        public event ConfigChangedEventHandler SelectedConfigChanged;
+        public event ConfigChangedEventHandler SelectedFormatChanged;
+
+        private void UpdateValueOrWarnError(TextBox senderBox, ref uint? valueToUpdate)
+        {
+            if (senderBox == null)
+                throw new ArgumentNullException(nameof(senderBox));
+            uint value;
+            if (!string.IsNullOrEmpty(senderBox.Text))
+            {
+                if (uint.TryParse(senderBox.Text, out value))
+                {
+                    valueToUpdate = value;
+                    senderBox.Foreground = Resources["TextBoxForegroundThemeBrush"] as Brush;
+                }
+                else
+                {
+                    senderBox.Foreground = new SolidColorBrush(Colors.Red);
+                }
+            }
+            else
+            {
+                valueToUpdate = null;
+            }
+        }
+
+        uint? audioBitsPerSample;
+        private void AudioBitsPerSampleBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateValueOrWarnError(sender as TextBox, ref audioBitsPerSample);
+        }
+
+        uint? audioSampleRate;
+        private void AudioSampleRateBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateValueOrWarnError(sender as TextBox, ref audioSampleRate);
+        }
+
+        uint? audioBitrate;
+        private void AudioBitrateBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateValueOrWarnError(sender as TextBox, ref audioBitrate);
+        }
+
+        uint? videoWidth;
+        private void VideoWidthBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateValueOrWarnError(sender as TextBox, ref videoWidth);
+        }
+
+        uint? videoHeight;
+        private void VideoHeightBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateValueOrWarnError(sender as TextBox, ref videoHeight);
+        }
+
+        uint? videoBitrate;
+        private void VideoBitrateBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            UpdateValueOrWarnError(sender as TextBox, ref videoBitrate);
+        }
     }
 }
