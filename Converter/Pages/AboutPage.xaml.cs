32d94eedfc30e72bdfa33d2cb07e90a9aecd9d57
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-03-13 11:33:30 +0800

    添加关于页面

---
+++ Converter/Pages/AboutPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace Converter.Pages
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class AboutPage : Page
+    {
+        private const string Email = "zhaobang.china@hotmail.com";
+        private const string AppId = "9ndjv6k3g2tj";
+
+        public AboutPage()
+        {
+            this.InitializeComponent();
+
+            appTitleBlock.Text = Package.Current.DisplayName;
+
+            var version = Package.Current.Id.Version;
+            versionBlock.Text = $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+
+            publisherBlock.Text = Package.Current.PublisherDisplayName;
+            installedDateBlock.Text = Package.Current.InstalledDate.ToString();
+
+            emailBlock.Text = Email;
+            emailButton.NavigateUri = new Uri($"mailto:{Email}");
+
+            feedbackCenterButton.Visibility =
+                Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.IsSupported() ?
+                Visibility.Visible : Visibility.Collapsed;
+
+            reviewButton.NavigateUri = new Uri($"ms-windows-store://review/?ProductId={AppId}");
+        }
+
+        private async void FeedbackCenterButton_Click(object sender, RoutedEventArgs e)
+        {
+            await Microsoft.Services.Store.Engagement.StoreServicesFeedbackLauncher.GetDefault().LaunchAsync();
+        }
+    }
+}
