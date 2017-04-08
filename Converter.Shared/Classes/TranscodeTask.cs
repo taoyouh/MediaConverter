d07f3172daa86a5fc217cb5040b00e43c3045cf9
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-04-08 14:30:18 +0800

    大规模重构：
    整理类名称；
    将TranscodeTaskManager拆分出一个ViewModel模块；
    将ViewModel放到共享项目中。

--- Converter.Shared/Classes/TranscodeTask.cs
+++ Converter.Shared/Classes/TranscodeTask.cs
@@ -1,34 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Media.MediaProperties;
 using Windows.Media.Transcoding;
 using Windows.Storage;
-using Windows.UI.Xaml;
 
 namespace Converter.Classes
 {
-    public class TranscodeTask : DependencyObject, INotifyPropertyChanging, INotifyPropertyChanged
+    public class TranscodeTask
     {
-        private const string Cancelled = "TranscodeStatus_Cancelled";
-        private const string Completed = "TranscodeStatus_Completed";
-        private const string Created = "TranscodeStatus_Created";
-        private const string Error = "TranscodeStatus_Error";
-        private const string InProgress = "TranscodeStatus_InProgress";
-        private const string Preparing = "TranscodeStatus_Preparing";
-        private const string ReadyToStart = "TranscodeStatus_ReadyToStart";
-        private const string Unknown = "TranscodeStatus_Unknown";
-
         private MediaTranscoder transcoder = new MediaTranscoder();
         private StorageFile source;
         private StorageFile destination;
-        private string outputFileName;
+        private string _outputFileName;
         private TranscodeConfiguration config;
         private PrepareTranscodeResult prepareResult;
+        private double _progress;
+        private TranscodeStatus _status;
+
 
         /// <summary>
         /// 创建TranscodeTask实例。
@@ -58,7 +49,7 @@ namespace Converter.Classes
 
             this.source = source;
             this.destination = destination;
-            outputFileName = this.destination.Name;
+            _outputFileName = this.destination.Name;
             this.config = config;
             _status = TranscodeStatus.Created;
             _progress = 0;
@@ -149,41 +140,33 @@ namespace Converter.Classes
             await destination.DeleteAsync(StorageDeleteOption.PermanentDelete);
         }
 
-        private async void TranscodeTask_ProgressChanged(IAsyncActionWithProgress<double> sender, double progress)
+        private void TranscodeTask_ProgressChanged(IAsyncActionWithProgress<double> sender, double progress)
         {
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                Progress = progress;
-            });
+            Progress = progress;
         }
 
         private async void TranscodeTask_Completed(IAsyncActionWithProgress<double> sender, AsyncStatus asyncStatus)
         {
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            switch (asyncStatus)
             {
-                switch (asyncStatus)
-                {
-                    case AsyncStatus.Completed:
-                        await destination.RenameAsync(outputFileName, NameCollisionOption.GenerateUniqueName);
-                        Status = TranscodeStatus.Completed;
-                        Progress = 100;
-                        break;
-                    case AsyncStatus.Error:
-                        Status = TranscodeStatus.Error;
-                        break;
-                    case AsyncStatus.Canceled:
-                        Status = TranscodeStatus.Cancelled;
-                        break;
-                }
-            });
+                case AsyncStatus.Completed:
+                    await destination.RenameAsync(_outputFileName, NameCollisionOption.GenerateUniqueName);
+                    Status = TranscodeStatus.Completed;
+                    Progress = 100;
+                    break;
+                case AsyncStatus.Error:
+                    Status = TranscodeStatus.Error;
+                    break;
+                case AsyncStatus.Canceled:
+                    Status = TranscodeStatus.Cancelled;
+                    break;
+            }
         }
 
         public string OutputFileName
         {
-            get { return outputFileName; }
+            get { return _outputFileName; }
         }
-
-        private double _progress;
 
         public double Progress
         {
@@ -196,14 +179,11 @@ namespace Converter.Classes
             {
                 if (_progress != value)
                 {
-                    PropertyChanging?.Invoke(this, new PropertyChangingEventArgs("Progress"));
                     _progress = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Progress"));
+                    ProgressChanged?.Invoke(this, value);
                 }
             }
         }
-
-        private TranscodeStatus _status;
 
         public TranscodeStatus Status
         {
@@ -216,14 +196,15 @@ namespace Converter.Classes
             {
                 if (_status != value)
                 {
-                    PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(Status)));
-                    PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(nameof(StatusString)));
                     _status = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StatusString)));
+                    StatusChanged?.Invoke(this, value);
                 }
             }
         }
+
+        public event EventHandler<double> ProgressChanged;
+
+        public event EventHandler<TranscodeStatus> StatusChanged;
 
         public enum TranscodeStatus
         {
@@ -235,36 +216,5 @@ namespace Converter.Classes
             Cancelled,
             Error
         }
-
-        public string StatusString
-        {
-            get
-            {
-                var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
-                switch (Status)
-                {
-                    case TranscodeStatus.Cancelled:
-                        return loader.GetString(Cancelled);
-                    case TranscodeStatus.Completed:
-                        return loader.GetString(Completed);
-                    case TranscodeStatus.Created:
-                        return loader.GetString(Created);
-                    case TranscodeStatus.Error:
-                        return loader.GetString(Error);
-                    case TranscodeStatus.InProgress:
-                        return loader.GetString(InProgress);
-                    case TranscodeStatus.Preparing:
-                        return loader.GetString(Preparing);
-                    case TranscodeStatus.ReadyToStart:
-                        return loader.GetString(ReadyToStart);
-                    default:
-                        return loader.GetString(Unknown);
-                }
-            }
-        }
-
-        public event PropertyChangingEventHandler PropertyChanging;
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
