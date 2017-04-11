2cf1e64961d72f432e0c65bc45b25dfd32bcdf66
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-04-11 19:55:15 +0800

    非master分支下只生成x86的包。

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -4,4 +4,11 @@ $destination = [System.IO.Path]::Combine($env:APPVEYOR_BUILD_FOLDER, "Build\Appx
 "nuget restore"
 nuget restore
 "msbuild"
-msbuild $projectPath /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:AppxBundlePlatforms="x86|x64|ARM" /p:AppxPackageDir=$destination /p:AppxBundle=Always /p:UapAppxPackageBuildMode=StoreUpload /p:configuration="release" /p:VisualStudioVersion="14.0"
+if ($env:APPVEYOR_REPO_BRANCH -eq "master")
+{
+    msbuild $projectPath /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:AppxBundlePlatforms="x86|x64|ARM" /p:AppxPackageDir=$destination /p:AppxBundle=Always /p:UapAppxPackageBuildMode=StoreUpload /p:configuration="release" /p:VisualStudioVersion="14.0"
+}
+else
+{
+    msbuild $projectPath /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:AppxBundlePlatforms="x86" /p:AppxPackageDir=$destination /p:AppxBundle=Always /p:UapAppxPackageBuildMode=StoreUpload /p:configuration="release" /p:platform="x86" /p:VisualStudioVersion="14.0"
+}
