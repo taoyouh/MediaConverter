3ffe9f982c2e0ca9f4e801bd7a6dc76a07fd6a6e
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-04-11 10:18:25 +0800

    在安装StoreBroker前先判断后删除文件夹。

--- Publish/InstallStoreBroker.ps1
+++ Publish/InstallStoreBroker.ps1
@@ -1,6 +1,9 @@
 ﻿"Cleaning..."
-$installFolder = $Home + "\Documents\WindowsPowerShell\Modules\StoreBroker"
-Remove-Item -Path $installFolder
+$installFolder = [System.IO.Path]::Combine($Home, "Documents\WindowsPowerShell\Modules\StoreBroker");
+if (Test-Path -Path $installFolder)
+{
+    Remove-Item -Force -Recurse -Path $installFolder
+}
 New-Item -Type Directory -Force -Path $installFolder
 
 "Cloning Store Broker"
