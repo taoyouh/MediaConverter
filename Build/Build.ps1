40eaae0091613b21748d46aecc04337a0ed9237d
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2017-10-28 10:41:47 +0800

    增加脚本的错误处理

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -2,7 +2,12 @@
 $destination = [System.IO.Path]::Combine($env:APPVEYOR_BUILD_FOLDER, "Build\AppxPackages")
 
 "nuget restore"
-nuget restore
+msbuild $projectPath /t:restore /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
 "msbuild"
 if ($env:APPVEYOR_REPO_BRANCH -eq "master")
 {
@@ -12,3 +17,4 @@ else
 {
     msbuild $projectPath /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:AppxBundlePlatforms="x86" /p:AppxPackageDir=$destination /p:AppxBundle=Always /p:UapAppxPackageBuildMode=StoreUpload /p:configuration="release" /p:platform="x86"
 }
+exit $LASTEXITCODE
